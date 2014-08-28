74ad2aa2da49de066f2bf433b7325bf27ca3847e
ckdarby <ckdarby@gmail.com>
2014-08-27 21:51:27 -0400

    Adding working queue page

--- src/GearmanMonitor/Controller/QueueController.php
+++ src/GearmanMonitor/Controller/QueueController.php
@@ -4,16 +4,22 @@ namespace GearmanMonitor\Controller;
 class QueueController
 {
 
-    public $queueService;
+    public $workerService;
+    public $twig;
+    public $servers;
 
-    public function __construct($twig, $queueService)
+    public function __construct($app)
     {
-        $this->queueService = $queueService;
-        $this->twig = $twig;
+        $this->queueService = $app['queue.service'];
+        $this->twig = $app['twig'];
+        $this->servers = $app['config.GearmanMonitor']['servers'];
     }
 
     public function indexAction()
     {
-        return [];
+        $servers = $this->queueService->getQueue($this->servers);
+        return $this->twig->render('queue.twig', [
+            'servers' => $servers
+        ]);
     }
 }
--- src/GearmanMonitor/Service/QueueService.php
+++ src/GearmanMonitor/Service/QueueService.php
@@ -1,11 +1,28 @@
 <?php
 namespace GearmanMonitor\Service;
+use Net\Gearman\Manager;
 
 class QueueService
 {
 
-    public function __construct()
+    /**
+     * Given a list of servers gets the current queue from them
+     *
+     * @param array Server addresses
+     * @return array Servers with appended queue info
+     */
+    public function getQueue($servers)
     {
+        foreach($servers as &$server){
+            try {
+                $germanManager = new Manager($server['address']);
+                $server['queue'] = $germanManager->status();
+            } catch (\Exception $e) {
+                //Unable to connect to server or workers unresponsive
+                $server['queue'] = [];
+            }
+        }
 
+        return $servers;
     }
 }
